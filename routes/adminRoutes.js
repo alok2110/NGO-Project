0a73b07e624d4785a515e1bf09631eaba57e2191
alok2110 <aloksaxena755@gmail.com>
2022-01-19 21:58:26 +0530

    change

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,5 +1,6 @@
 const app = require("express");
 const router = app.Router();
+const auth = require("../utils/auth");
 const {
   register,
   login,
@@ -12,10 +13,10 @@ const {
 
 router.post("/registerAdmin", register);
 router.post("/loginAdmin", login);
-router.get("/viewRegisteredUser", viewRegisteredUser);
-router.get("/approveUser/:id", approveUser);
-router.get("/rejectUser/:id", rejectUser);
-router.get("/viewApprovedUser", viewApprovedUser);
-router.get("/viewRejectedUser", viewRejectedUser);
+router.get("/viewRegisteredUser", auth, viewRegisteredUser);
+router.get("/approveUser/:id", auth, approveUser);
+router.get("/rejectUser/:id", auth, rejectUser);
+router.get("/viewApprovedUser", auth, viewApprovedUser);
+router.get("/viewRejectedUser", auth, viewRejectedUser);
 
 module.exports = router;
